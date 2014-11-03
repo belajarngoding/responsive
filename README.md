349379eebce3abb3fc837bddd34e70863b2cdec9
cwicaksono <cahyo@the4id.com>
2014-11-03 22:39:00 +0700

    Initial commit and adding readme

---
+++ README.md
@@ -0,0 +1,15 @@
+Blog menggunakan PHP dan MySQL
+=========
+
+Ini merupakan source code untuk tulisan pada
+
+```sh
+http://belajarngoding.com/membuat-responsive-website/
+```
+
+Implementasi sederhana pengimplementasian responsive design. 
+
+License
+=========
+MIT
+
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<head profile="http://gmpg.org/xfn/11">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Responsive Example By belajarngoding.com</title>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	<meta name="viewport" content="width=device-width" />
+
+	<!-- Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+<body>
+
+<div class="header">
+	<h1>Responsive Example</h1>
+	<p>by <a href="http://belajarngoding.com/?ref=responsive-example">belajarngoding.com</a></p>
+</div>
+
+<div class="container clearfix">
+	<div class="content">
+		<div class="item">
+			<p><strong>This is content area</strong></p>
+			<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
+		</div>
+	</div>
+	<div class="sidebar">
+		<div class="item">
+			<p><strong>Sidebar</strong></p>
+			<p>This is sidebar area, you can fill it with widget or simple information like about me or other content.</p>
+		</div>
+	</div>
+</div>
+
+<div class="footer">
+	<p>Respect the author - <a href="http://belajarngoding.com/?ref=responsive-example">belajarngoding.com</a></p>
+</div>
+
+</body>
+</html>
